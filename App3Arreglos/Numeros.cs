f5bd50b6a3834fb28df5db16618d90bef4ae5f6d
ElizabethLlanos <85859478+ElizabethLlanos@users.noreply.github.com>
2021-06-22 16:06:32 -0500

    App_Arreglos_20Num
    Este programa recibe 20 números y muestra la lista de los números leídos, seguida de la lista de los números ordenados de menor a mayor.

---
+++ App3Arreglos/Numeros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3Arreglos
+{
+    class Numeros
+    {
+        //Método para ordenar números
+        public int[] Orden_Menor_mayor(int[] num)
+        {
+            int[] numeros = new int[20];
+            numeros = num;
+            //Se ordenan los números de menor a mayor
+            Array.Sort(numeros);
+            return numeros;
+        }
+    }
+}
---
+++ App3Arreglos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace App3Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia
+            Numeros objNumero = new Numeros();
+            //Arreglo
+            int[] numero = new int[20];
+            Console.WriteLine("►►►►► NÚMEROS ENTEROS◄◄◄◄◄");
+            Console.WriteLine("♦Introduce 20 numeros: ");
+            //Se reciben los números y se guardan en el arreglo
+            for(int i = 0; i < 20; i++)
+            {
+                Console.Write("►");
+                numero[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Los números introducidos son:");
+            //se recorre el array
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"► {numero[i]}");
+               
+            }
+            Console.WriteLine("►►► Números ordenados de menor a mayor ◄◄◄");
+            //Llamada al método para ordenar los numeros
+            numero = objNumero.Orden_Menor_mayor(numero);
+            //Recorrer el array ya modificado
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"► {numero[i]}");
+
+            }
+        }
+    }
+}
